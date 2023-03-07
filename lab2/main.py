67ae34aaef665b4526bdd3fdc9915aa677360ce7
krzysztofnoga1 <krzysztofnoga83@gmail.com>
2023-03-07 18:43:03 +0100

    compilation problem fixed

--- lab2/main.py
+++ lab2/main.py
@@ -3,28 +3,28 @@
 import sys
 
 def string_to_numeric(count, str):
-	index=0;
-	pw=power(10, count-1)
-	res=0
-	while str[index] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-		res+=pw*int(str[index])
-		pw=pw/10
-		index=index+1
-	return res
+    index=0;
+    pw=power(10, count-1)
+    res=0
+    while str[index] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        res+=pw*int(str[index])
+        pw=pw/10
+        index=index+1
+    return res
 
 def get_string_length(str):
-	index=0
-	count=0
+    index=0
+    count=0
 	
-	if(str[index] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']):
-		return -1, count
-	while(str[index] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']):
-		count=count+1
-		index=index+1
-	if(str[index]!='k'):
-		return -1, count
-	num=string_to_numeric(count, str)
-	return num, count
+    if(str[index] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        return -1, count
+    while(str[index] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        count=count+1
+        index=index+1
+    if(str[index]!='k'):
+        return -1, count
+    num=string_to_numeric(count, str)
+    return num, count
 
 def my_printf(format_string,param):
     #print(format_string)
@@ -32,12 +32,12 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == '.':
-            	length, count=get_string_length(format_string[idx+2:])
-            	length=int(length)
-            	if length>0:
-            		print(param[:int(length)].swapcase(), end="")
+                length, count=get_string_length(format_string[idx+2:])
+                length=int(length)
+                if length>0:
+            	    print(param[:int(length)].swapcase(), end="")
                 shouldDo=False
-                print(format_string[idx+3+count:], end="")
+                print(format_string[idx+3+count:],end="")
             else:
                 print(format_string[idx],end="")
         else:
