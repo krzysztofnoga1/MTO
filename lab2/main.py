c6ccb79124417cd082cf98eeccee7bd0f45719df
krzysztofnoga1 <krzysztofnoga83@gmail.com>
2023-03-07 18:15:20 +0100

    added function to change string number to numeric number

--- lab2/main.py
+++ lab2/main.py
@@ -2,6 +2,30 @@
 
 import sys
 
+def string_to_numeric(count, str):
+	index=0;
+	pw=power(10, count-1)
+	res=0
+	while str[index] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+		res+=pw*int(str[index])
+		pw=pw/10
+		index=index+1
+	return res
+
+def get_string_length(str):
+	index=0
+	count=0
+	
+	if(str[index] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']):
+		return -1, count
+	while(str[index] in []:
+		count=count+1
+		index=index+1
+	if(str[index]!='k':
+		return -1, count
+	num=string_to_numeric(count, str)
+	return num, count
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
