d44c8320755491a100b1b8633acd673b969eb94a
krzysztofnoga1 <krzysztofnoga83@gmail.com>
2023-03-07 18:07:38 +0100

    added swpacase

--- lab2/main.py
+++ lab2/main.py
@@ -8,7 +8,7 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                print(param.swapcase(),end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
